36d7e8fc2e0801939925745a2947169afa04932f
m-buschmann <snep114@gmail.com>
2025-07-07 14:14:18 +0200

    feat: 6 synapses at each dendrite

--- brian_with_cables.py
+++ brian_with_cables.py
@@ -4,78 +4,13 @@ prefs.codegen.target = "numpy"   # Optional; makes things run without Cython
 
 defaultclock.dt = 0.01*ms
 
-def excite_one_dendrite(dendrite):
-    """
-    ATTENTION: not up to date, use excite_both_dendrites() instead.
-    Excite one of the dendrites (0 for left, 1 for soma, 2 for right).
-    dendrite: int, index of the dendrite to excite (0 for left, 1 for soma, 2 for right)
-    """
-
+def excite_both_dendrites(timel = [10,11,12], timer = [13,14,15]):
     start_scope()
 
     # Morphology: L (left dendrite) - soma - R (right dendrite)
     morpho = Soma(diameter=30*um)
-    morpho.L = Cylinder(length=50*um, diameter=3*um, n=1)
-    morpho.R = Cylinder(length=50*um, diameter=3*um, n=1)
-
-    eqs = '''
-    Im = gl * (El - v) : amp/meter**2
-    gl : siemens/meter**2
-    El : volt
-    '''
-
-    neuron = SpatialNeuron(
-        morphology=morpho,
-        model=eqs,
-        Cm=1*uF/cm**2,
-        Ri=100*ohm*cm,
-        method='exponential_euler',
-    )
-
-    neuron.v = -65*mV
-    neuron.gl = 0.0003*siemens/cm**2
-    neuron.El = -65*mV
-
-    # Input group: spike train at given times
-    spiketimes = [50, 75, 100, 200]*ms
-    input = SpikeGeneratorGroup(1, [0]*len(spiketimes), spiketimes)
-
-    # Connect input to the chosen dendrite
-    syn = Synapses(input, neuron, on_pre='v_post += 2*mV')
-    syn.connect(i=0, j=dendrite)
-
-    M = StateMonitor(neuron, 'v', record=True)
-    run(500*ms)
-
-    plt.plot(M.t/ms, M.v[0]/mV, label='dendL')
-    plt.plot(M.t/ms, M.v[1]/mV, label='soma')
-    plt.plot(M.t/ms, M.v[2]/mV, label='dendR')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('v (mV)')
-    plt.legend()
-    plt.title(f'Stimulus on dendrite index {dendrite}')
-    plt.show()
-
-    locations = neuron.distance / um  # or .x for "x coordinate"
-    plt.figure(figsize=(10, 4))
-    plt.imshow(M.v / mV, aspect='auto',
-               extent=[M.t[0] / ms, M.t[-1] / ms, locations[-1], locations[0]],
-               cmap="viridis")
-    plt.colorbar(label='Membrane potential (mV)')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Distance from soma ($\mu$m)')
-    plt.title('Space–time voltage map')
-    plt.show()
-
-
-
-def excite_both_dendrites(time1 = [10,11,12], time2 = [13,14,15]):
-    start_scope()
-
-    # Morphology: L (left dendrite) - soma - R (right dendrite)
-    morpho = Soma(diameter=30*um)
-    morpho.L = Cylinder(length=50*um, diameter=10*um, n=1)
-    morpho.R = Cylinder(length=50*um, diameter=10*um, n=1)
+    morpho.L = Cylinder(length=50*um, diameter=5*um, n=1)
+    morpho.R = Cylinder(length=50*um, diameter=5*um, n=1)
 
     eqs = '''
     Im = gl * (El - v) : amp/meter**2
@@ -99,19 +34,25 @@ def excite_both_dendrites(time1 = [10,11,12], time2 = [13,14,15]):
     neuron.gl = 0.00005*siemens/cm**2  # tau=20 ms
     neuron.El = -65*mV
 
-    # Input group: spike train at given times
-    spiketimes1 = time1*ms
-    input1 = SpikeGeneratorGroup(1, [0]*len(spiketimes1), spiketimes1)
+    # Generate 6 spike times for 6 axons arriving at the same time (say, t=10 ms)
+    left_spiketimes = timel*ms
+    right_spiketimes = timer*ms
 
-    spiketimes2 = time2*ms
-    input2 = SpikeGeneratorGroup(1, [0]*len(spiketimes2), spiketimes2)
+    #indices are lists each containing n=timel and n=timer axons
+    left_indices = list(range(len(timel)))  
+    right_indices = list(range(len(timer)))  
 
-    # Connect input to the chosen dendrite
-    syn1 = Synapses(input1, neuron, on_pre='v_post += 3*mV')
-    syn1.connect(i=0, j=0)
+    # Create input groups (6 axons each)
+    input_left = SpikeGeneratorGroup(len(timel), left_indices, left_spiketimes)
+    input_right = SpikeGeneratorGroup(len(timer), right_indices, right_spiketimes)
 
-    syn2 = Synapses(input2, neuron, on_pre='v_post += 3*mV')
-    syn2.connect(i=0, j=2)
+    # Synapses: connect ALL left inputs to dendrite 0; right inputs to dendrite 2
+    syn_left = Synapses(input_left, neuron, on_pre='v_post += 1*mV')
+    syn_left.connect(i=range(len(timel)), j=0)  # all to compartment 0
+
+    syn_right = Synapses(input_right, neuron, on_pre='v_post += 1*mV')
+    syn_right.connect(i=range(len(timer)), j=2)  # all to compartment 2
+
 
     M = StateMonitor(neuron, 'v', record=True)
     spikemon = SpikeMonitor(neuron)
@@ -147,7 +88,8 @@ def excite_both_dendrites(time1 = [10,11,12], time2 = [13,14,15]):
 
 def main():
     # Excite the first dendrite
-    excite_both_dendrites(time1=[5.1,5.2,5.3,5.4,5.5,5.6,5.7], time2=[6.1,6.2,6.3,6.4,6.5,6.6,6.7])
+    #excite_both_dendrites(timel=[5.1,5.2,5.3,5.4,5.5,5.6], timer=[6.1,6.2,6.3,6.4,6.5,6.6])
+    excite_both_dendrites(timel=[5,5,5,5,5,5], timer=[6,6,6,6,6,6])
 
     # Excite only the second dendrite
     #excite_one_dendrite(2)
